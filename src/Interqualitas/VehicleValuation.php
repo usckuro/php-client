187d0a7b5eaf785dac4411c4c1add6fc13344440
Jon Wadsworth <jonrwads@gmail.com>
2015-02-18 18:43:06 -0800

    Fixing incorrectly named class

--- src/Interqualitas/VehicleValuation.php
+++ src/Interqualitas/VehicleValuation.php
@@ -8,7 +8,8 @@ use Interqualitas\ModuleAbstract;
  * The API Calls to interact with vehicle valuations
  * @author Jon Wadsworth <jon@interqualitas.net>
  */
-class VehicleValuations extends ModuleAbstract{
+class VehicleValuation
+extends ModuleAbstract{
     public function __construct(\Interqualitas $main) {
         parent::__construct($main);
         $this->modulePath = 'api/vehiclevaluation';
